0ab9d977311cf04912585ca123ca570e25244fb3
nirmal3002 <nirmal.m3002@gmail.com>
2024-09-17 04:11:18 +0000

    the updated code of express project

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const bookRoutes = require('./routes/bookRoutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
