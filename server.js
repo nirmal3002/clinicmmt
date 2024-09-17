ced263dbc4414f5ac9f5863f69c2f9df6ddeaa16
nirmal3002 <nirmal.m3002@gmail.com>
2024-09-17 09:52:44 +0000

    added get post update in backend prog

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
     res.send("HomePage Of The App");
 });
 
-
+app.use('/api', bookRoutes); 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
